38e10e203f2aad86777aa21cd228e5ef78e57f4b
HUUNHAT <huunhatth07@gmail.com>
2018-11-07 17:42:54 +0700

    Create model

---
+++ HunaShop.Model/Models/Footer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HunaShop.Model.Models
+{
+    [Table("Footers")]
+    public class Footer
+    {
+        [Key]
+        public String ID { get; set; }
+
+        [Required]
+        public String Content { get; set; }
+    }
+}
